fdeab97512a1a6570e696f9a1d0fad4351788100
matteo <matteo@pop-os.localdomain>
2021-03-16 03:11:40 -0700

    added dynamic content and cover photo

---
+++ daVinci.js
@@ -0,0 +1,14 @@
+const content = [
+    {
+        title: 'Mona Lisa',
+        attribute: 'Leonardo da Vinci',
+        date: '1503-1506',
+        img: './img/src/monalisa',
+        content: `The painting is probably of the Italian noblewoman Lisa Gherardini, 
+        the wife of Francesco del Giocondo, and is in oil on a white Lombardy poplar 
+        panel. It had been believed to have been painted between 1503 and 1506; 
+        however, Leonardo may have continued working on it as late as 1517. It was 
+        acquired by King Francis I of France and is now the property of the French 
+        Republic itself, on permanent display at the Louvre, Paris since 1797.`
+    }
+]
---
+++ index.js
@@ -0,0 +1,58 @@
+const database = [
+    {
+        title: 'Mona Lisa',
+        attribute: 'Leonardo da Vinci',
+        date: '1503-1506',
+        img: './img/src/monalisa.jpeg',
+        text: `The painting is probably of the Italian noblewoman Lisa Gherardini, 
+        the wife of Francesco del Giocondo, and is in oil on a white Lombardy poplar 
+        panel. It had been believed to have been painted between 1503 and 1506; 
+        however, Leonardo may have continued working on it as late as 1517. It was 
+        acquired by King Francis I of France and is now the property of the French 
+        Republic itself, on permanent display at the Louvre, Paris since 1797.`
+    }
+]
+
+
+
+function generateContent() {
+    
+    const contentContainer = document.createElement('div');
+    contentContainer.setAttribute('class', 'container')
+    document.body.appendChild(contentContainer)
+
+    for (i = 0; i < database.length; i++) {
+        
+        // Generates Image
+        const img = document.createElement('img');
+        img.setAttribute('src', database[i].img);
+        img.setAttribute('class', 'img');
+        contentContainer.appendChild(img);
+        
+        // Generates Title
+        const title = document.createElement('h2');
+        title.innerHTML = database[i].title;
+        title.setAttribute('class', 'title');
+        contentContainer.appendChild(title);
+
+        // Generates Attribute
+        const attribute = document.createElement('h4');
+        attribute.innerHTML = database[i].attribute;
+        attribute.setAttribute('class', 'attribute');
+        contentContainer.appendChild(attribute);
+
+        // Generates Date
+        const date = document.createElement('h4');
+        date.innerHTML = database[i].date;
+        date.setAttribute('class', 'date');
+        contentContainer.appendChild(date);
+        
+        // Generates Text Content
+        const text = document.createElement('p');
+        text.innerHTML = database[i].text;
+        text.setAttribute('class', 'text');
+        contentContainer.appendChild(text);   
+    }
+}
+
+generateContent();
